e02586a8f1373bba6328c391f5abc6c557354dfc
bryan lesht <bryan.lesht@colorado.edu>
2020-01-23 18:57:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,11 @@
 def square_array(array)
   i = 0 
+  new_numbers = []
   while array[i] do
-    array[i] = array[i]**2 
+    new_numbers<<array[i]**2
     i += 1
   end
-  puts array
+  puts new_numbers
 end
   
   
